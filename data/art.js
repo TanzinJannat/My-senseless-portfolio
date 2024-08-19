73ac62719d00cc51b964d2e5835644aaf07e3b16
Tanzin <161127740+TanzinJannat@users.noreply.github.com>
2024-08-20 01:28:58 +0600

    Update art.js

--- data/art.js
+++ data/art.js
@@ -2,7 +2,7 @@ const products = [{
     image: "/Images/Art/Art-1.jpg",
     name: "Bamboo garden - Acrylic on canvas"
 },{
-    image: "../Images/Art/Art-2.jpg",
+    image: "Images/Art/Art-2.jpg",
     name: "Ghibli style - Acrylic on canvas"
 },{
     image: "../Images/Art/Art-3.jpg",
