baa64e51d85a82f724c55021f929ddbda434f4df
Tanzin <161127740+TanzinJannat@users.noreply.github.com>
2024-08-20 16:17:17 +0600

    Update photos.js

--- data/photos.js
+++ data/photos.js
@@ -1,7 +1,7 @@
 const products = [{
-    image: "../Images/Photos/photo-1.jpg",
+    image: "Images/Photos/photo-1.jpg",
     name: "June-27,2024 - A Haldi night"
 }, {
-    image: "../Images/Photos/photo-2.jpg",
+    image: "Images/Photos/photo-2.jpg",
     name: "November-10,2018 - A memory with my best friend"
 }];
