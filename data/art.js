0969f455c733e5cb4b80d183fb147b7c90090363
Tanzin <161127740+TanzinJannat@users.noreply.github.com>
2024-08-20 15:52:14 +0600

    Update art.js

--- data/art.js
+++ data/art.js
@@ -1,13 +1,13 @@
 const products = [{
-    image: "/Images/Art/Art-1.jpg",
+    image: "Images/Art/Art-1.jpg",
     name: "Bamboo garden - Acrylic on canvas"
 },{
     image: "Images/Art/Art-2.jpg",
     name: "Ghibli style - Acrylic on canvas"
 },{
-    image: "../Images/Art/Art-3.jpg",
+    image: "Images/Art/Art-3.jpg",
     name: "Ghibli style - Acrylic on canvas"
 },{
-    image: "../Images/Art/Art-4.jpg",
+    image: "Images/Art/Art-4.jpg",
     name: "Starry night - Acrylic on canvas"
 }];
