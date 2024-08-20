98830aa2d2b0890a35e01525b4eb36950204bcf0
Tanzin <161127740+TanzinJannat@users.noreply.github.com>
2024-08-20 16:30:09 +0600

    Update Illustration-products.js

--- data/Illustration-products.js
+++ data/Illustration-products.js
@@ -1,19 +1,19 @@
 const products = [{
-    image: '../Images/Illustrations/Illustration-1.png',
+    image: 'Images/Illustrations/Illustration-1.png',
     name: 'Every flower blooms differently - Digital'
 }, {
-    image: '../Images/Illustrations/Illustration-2.png',
+    image: 'Images/Illustrations/Illustration-2.png',
     name: ' Head in the sky - Digital'
 }, {
-    image: '../Images/Illustrations/Illustration-3.png',
+    image: 'Images/Illustrations/Illustration-3.png',
     name: 'Flowers are blooming - Digital'
 }, {
-    image: '../Images/Illustrations/Illustration-4.jpg',
+    image: 'Images/Illustrations/Illustration-4.jpg',
     name: 'A dream picnic - Digital'
 }, {
-    image: '../Images/Illustrations/Illustration-5.jpg',
+    image: 'Images/Illustrations/Illustration-5.jpg',
     name:  'Garden with butterflies - Digital '
 },{
-    image: '../Images/Illustrations/Illustration-6.jpg',
+    image: 'Images/Illustrations/Illustration-6.jpg',
     name:  'Proud parents - Digital '
 }];
